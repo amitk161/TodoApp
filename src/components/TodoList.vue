235f62a85b026ac5e7dd3757ce1a2a1aacb4db21
amitk161 <a.kamitkumar1920@gmail.com>
2024-03-02 21:42:23 +0530

    Added todo app files

---
+++ src/components/TodoList.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <p class="display-3">Notes App</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <NewTodo @on-addtodo="addTodo($event)"/>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <ul class="list-group">
+                    <TodoSingle v-for="(todo, index) in todos" :key="index" 
+                    :todoString="todo.todoString"
+                    :completed="todo.completed"
+                    @on-delete="deleteTodo(todo)"
+                    @on-toggle="toggleTodo(todo)"
+                    @on-edit="editTodo(todo, $event)"
+                    />
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoSingle from './TodoSingle.vue';
+import NewTodo from './NewTodo.vue'
+export default {
+    components:{
+        TodoSingle,
+        NewTodo
+    },
+    data() {
+        return {
+            todos: [
+                {todoString: "Make coffee", completed: false},
+                {todoString: "Make Maggi", completed: true},
+                {todoString: "Make Dosa", completed: false},
+                {todoString: "Make Eggs", completed: true},
+            ]
+        }
+    },
+    methods: {
+        addTodo(newTodo){
+            this.todos.push({
+                todoString: newTodo,
+                completed: false
+            })
+        },
+        toggleTodo(todo){
+            todo.completed = !todo.completed;
+        },
+        editTodo(todo, newTodoString){
+            todo.todoString = newTodoString;
+        },
+        deleteTodo(deleteTodo){
+            this.todos = this.todos.filter(
+                todo => todo.todoString !== deleteTodo.todoString);
+        }
+    }
+}
+</script>
+
+<style >
+
+</style>
